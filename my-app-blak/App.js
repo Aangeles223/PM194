e5989cf0d5a3cd0301faf71185ad07d47e836bdf
Aangeles223 <122044096@upq.edu.mx>
2025-06-11 19:07:47 -0600

    Props y States

--- my-app-blak/App.js
+++ my-app-blak/App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* ZONA 1 : IMPORTACIONES */
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const Texto = (props) => {
+  const { contenidos } = props;
+  return <Text> {contenidos} </Text>;
+};
+
+/* ZONA 2 : MAIN */
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenidos="Hola"> </Texto>
+      <Texto contenidos="Mundo"> </Texto>
+      <Texto contenidos="Reac Native"></Texto>
+      <Button title="Trabaja!"></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* ZONA 3 : ESTILOS */
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
